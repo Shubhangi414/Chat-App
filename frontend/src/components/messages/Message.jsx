cb0446bb7e12ac507ec2ce2173a01041ae84b850
Shubhangi <shubhangi@softobiz.com>
2024-03-14 16:05:21 +0530

    SOCKET-IO IMPLEMENTED FOR Real Time Chat and Implemented the online status and sound function

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import { useAuthContext } from '../../context/authContext'
-
-import useConversation from '../../zustand/useConversation'
-import { extractTime } from '../../utils/extractTime';
+import { useAuthContext } from "../../context/authContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
 
 const Message = ({ message }) => {
-    const { authUser } = useAuthContext();
+	const { authUser } = useAuthContext();
 	const { selectedConversation } = useConversation();
 	const fromMe = message.senderId === authUser._id;
 	const formattedTime = extractTime(message.createdAt);
 	const chatClassName = fromMe ? "chat-end" : "chat-start";
 	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
 	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
-    const shakeClass = message.shouldShake ? "shake" : "";
+	const shakeClass = message.shouldShake ? "shake" : "";
 
-    return (
-        <div className={`chat ${chatClassName}`}>
+	return (
+		<div className={`chat ${chatClassName}`}>
 			<div className='chat-image avatar'>
 				<div className='w-10 rounded-full'>
 					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
@@ -24,7 +22,6 @@ const Message = ({ message }) => {
 			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
 			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
 		</div>
-            )
-}
-
-            export default Message
+	);
+};
+export default Message;
